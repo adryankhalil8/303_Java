610d868cf678d0acef1b03907e37d9b3053f07e7
adryankhalil8 <adryan.jefferson@peopleshores.com>
2026-03-05 12:39:08 -0500

    GLAB303.11.5

---
+++ lessons/assignments/303.11/GLAB 303.11.5/TreesetExampletwo.java
@@ -0,0 +1,34 @@
+import java.util.TreeSet;
+public class TreesetExampletwo {
+    public static void main(String[] args) {
+        // Example 2 Remove Elements
+//        TreeSet<Integer> numbers = new TreeSet<>();
+//        numbers.add(2);
+//        numbers.add(5);
+//        numbers.add(6);
+//        System.out.println("TreeSet: " + numbers);
+//
+//        // Using the remove() method
+//        boolean value1 = numbers.remove(5);
+//        System.out.println("Is 5 removed? " + value1);
+//
+//        // Using the removeAll() method
+//        boolean value2 = numbers.removeAll(numbers);
+//        System.out.println("Are all elements removed? " + value2);
+//    }
+        // first () and last()
+        TreeSet<Integer> numbers = new TreeSet<>();
+        numbers.add(2);
+        numbers.add(5);
+        numbers.add(6);
+        System.out.println("TreeSet: " + numbers);
+
+        // Using the first() method
+        int first = numbers.first();
+        System.out.println("First Number: " + first);
+
+        // Using the last() method
+        int last = numbers.last();
+        System.out.println("Last Number: " + last);
+    }
+}
