ce14e9bbea541d38ba559c8ee2ccc20e568fae2c
adryankhalil8 <adryan.jefferson@peopleshores.com>
2026-03-05 10:41:01 -0500

    303.11.1 Completed correctly

--- lessons/assignments/303.11/GLAB 303.11.1/DemoClass.java
+++ lessons/assignments/303.11/GLAB 303.11.1/DemoClass.java
@@ -1,9 +1,9 @@
-public class DemoClass {
-
-    // create a generics method
-    public <T> void genericsMethod(T data) {
-        System.out.println("Generics Method:");
-        System.out.println("Data Passed: " + data);
-    }
-}
+//public class DemoClass {
+//
+//    // create a generics method
+//    public <T> void genericsMethod(T data) {
+//        System.out.println("Generics Method:");
+//        System.out.println("Data Passed: " + data);
+//    }
+//}
 
