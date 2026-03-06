e5366ebc267e80726043aa4bff6c65f7d2688aa0
adryankhalil8 <adryan.jefferson@peopleshores.com>
2026-03-06 16:08:02 -0500

    PA303.10.1

---
+++ lessons/assignments/303.10/PA303.10.1/Monster.java
@@ -0,0 +1,11 @@
+public class Monster {
+    protected String name;
+
+    public Monster(String name) {
+        this.name = name;
+    }
+
+    public String attack() {
+        return "!^_&^$@+%$* I don't know how to attack!";
+    }
+}
